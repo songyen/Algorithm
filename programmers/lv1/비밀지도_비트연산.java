40624789fb91e880c6f0fbf29e2c0c5697600eed
songyen <yeeun4150@gmail.com>
2021-03-02 23:22:56 +0900

    update Programmers[비밀지도_비트연산]

--- programmers/lv1/비밀지도_비트연산.java
+++ programmers/lv1/비밀지도_비트연산.java
@@ -3,21 +3,12 @@ package lv1;
 class 비밀지도_비트연산 {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
-        int[] map = new int[n];
         for(int i=0;i<n;i++){
-            map[i] = arr1[i] | arr2[i];
-        }
-
-        for(int i=0;i<n;i++){
-            answer[i] = "";
-            for(int j=1<<n-1;j>=1; j = j>>1){
-                if((map[i] & j) == j){
-                    answer[i] += "#";
-                }
-                else{
-                    answer[i] += " ";
-                }
-            }
+            String tmp = Integer.toBinaryString(arr1[i] | arr2[i]);
+            tmp = String.format("%"+n+"s",tmp);
+            tmp = tmp.replaceAll("1","#");
+            tmp = tmp.replaceAll("0"," ");
+            answer[i] = tmp;
         }
 
         return answer;
