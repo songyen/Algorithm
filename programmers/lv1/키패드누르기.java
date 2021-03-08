d7ef0cbd92956b3626bcbfe80ea8601a28cd3256
songyen <yeeun4150@gmail.com>
2021-03-08 22:25:33 +0900

    update Programmers [키패드누르기] 평균 3.5초 -> 0초 미만

--- programmers/lv1/키패드누르기.java
+++ programmers/lv1/키패드누르기.java
@@ -1,83 +1,60 @@
 package lv1;
 
-import java.util.Scanner;
 
 class 키패드누르기 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
         String hand = "right";
         System.out.println(solution(numbers,hand));
     }
 
     public static String solution(int[] numbers, String hand) {
-        String answer = "";
-        int[][] keypad = {{1,2,3},
-                {4,5,6},
-                {7,8,9},
-                {10,0,11}};
-        int leftX = 3, leftY = 0;
-        int rightX =  3, rightY = 2;
-        int press = 0;
-        int x = 0;
-        int y = 0;
+        StringBuilder answer = new StringBuilder();
+        int leftLocation = 10;
+        int rightLocation = 12;
+        int leftDistance = 0;
+        int rightDistance = 0;
 
-        while(press!=numbers.length){
-            Loop1:
-            for(int i=0;i<4;i++){
-                Loop2:
-                for(int j=0;j<3;j++){
-                    if(keypad[i][j] == numbers[press]) {
-                        if ((numbers[press] - 1) % 3 == 0) {
-                            //왼손
-                            answer += "L";
-                            leftX = i;
-                            leftY = j;
-                            break Loop1;
-                        } else if (numbers[press] % 3 == 0 && numbers[press]!=0) { //3 6 9
-                            //오른손
-                            answer += "R";
-                            rightX = i;
-                            rightY = j;
-                            break Loop1;
-                        } else {
-                            x = i;
-                            y = j;
+        for (int number : numbers) {
+            if ((number - 1) % 3 == 0) {
+                answer.append("L");
+                leftLocation = number;
+            } else if (number % 3 == 0 && number!=0) {
+                answer.append("R");
+                rightLocation = number;
+            } else {
+                leftDistance = getDistance(leftLocation, number);
+                rightDistance = getDistance(rightLocation, number);
 
-                            if (Math.abs(leftX - x) + Math.abs(leftY - y) > Math.abs(rightX - x) + Math.abs(rightY - y)) {
-                                //오른손
-                                answer += "R";
-                                rightX = x;
-                                rightY = y;
-                                break Loop1;
-                            } else if (Math.abs(leftX - x) + Math.abs(leftY - y) < Math.abs(rightX - x) + Math.abs(rightY - y)) {
-                                //왼손
-                                answer += "L";
-                                leftX = x;
-                                leftY = y;
-                                break Loop1;
-                            } else {
-                                if (hand.equals("right")) {
-                                    //오른손잡이
-                                    answer += "R";
-                                    rightX = x;
-                                    rightY = y;
-                                    break Loop1;
-                                } else {
-                                    //왼손잡이
-                                    answer += "L";
-                                    leftX = x;
-                                    leftY = y;
-                                    break Loop1;
-                                }
-                            }
-                        }
+                if (leftDistance > rightDistance) {
+                    answer.append("R");
+                    rightLocation = number;
+                } else if (leftDistance < rightDistance) {
+                    answer.append("L");
+                    leftLocation = number;
+                } else {
+                    if (hand.equals("right")) {
+                        answer.append("R");
+                        rightLocation = number;
+                    } else {
+                        answer.append("L");
+                        leftLocation = number;
                     }
                 }
             }
-            press++;
         }
+        return answer.toString();
+    }
 
-        return answer;
+    public static int getDistance(int location, int number){
+        if(number == 0) number = 11;
+        if(location == 0) location = 11;
+
+        int targetX = (number-1)/3;
+        int targetY = (number-1)%3;
+        int locationX = (location-1)/3;
+        int locationY = (location-1)%3;
+
+        return Math.abs(targetX-locationX) + Math.abs(targetY-locationY);
     }
 }
