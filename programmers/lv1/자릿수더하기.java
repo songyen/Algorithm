b5354731787091f9f648b4947bc65b0343022770
songyen <yeeun4150@gmail.com>
2021-03-16 20:31:57 +0900

    Programmers [자릿수더하기]

---
+++ programmers/lv1/자릿수더하기.java
@@ -0,0 +1,13 @@
+package lv1;
+
+public class 자릿수더하기 {
+    public int solution(int n) {
+        int answer = 0;
+        while(n!=0){
+            answer += n%10;
+            n /= 10;
+        }
+
+        return answer;
+    }
+}
