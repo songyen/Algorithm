96582f869b4685750e489a302b34cc42b863907e
songyen <yeeun4150@gmail.com>
2021-03-31 23:18:20 +0900

    update Programmers [크레인인형뽑기게임] list -> stack 다른 풀이 추가

--- programmers/lv1/크레인인형뽑기게임.java
+++ programmers/lv1/크레인인형뽑기게임.java
@@ -1,3 +1,6 @@
+/*
+*reference : https://velog.io/@ajufresh/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%ED%81%AC%EB%A0%88%EC%9D%B8-%EC%9D%B8%ED%98%95%EB%BD%91%EA%B8%B0-%EA%B2%8C%EC%9E%84-%EB%AC%B8%EC%A0%9C%ED%92%80%EC%9D%B4-Java
+ */
 package lv1;
 
 import java.util.*;
@@ -8,6 +11,34 @@ public class 크레인인형뽑기게임 {
         int[] moves = {1,5,3,5,1,2,1,4};
         System.out.println(solution(board, moves));
     }
+
+    //stack이용한 풀이. stack.peek()비교후 stack.pop() or stack.push()하는 방법으로 풀이.
+    public static int solution(int[][] board, int[] moves) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        int count = 0;
+
+        for(int move : moves){
+            for(int i=0;i<board.length;i++){
+                if(board[i][move-1] != 0){
+                    if(stack.peek() == board[i][move-1]){
+                        stack.pop();
+                        count += 2;
+                    }else{
+                        stack.push(board[i][move-1]);
+                    }
+
+                    board[i][move-1] = 0;
+                    break;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    /*
+    //list이용한 풀이, box에 넣고 비교후 삭제하는 방법으로 풀이.
     public static int solution(int[][] board, int[] moves) {
         List<Integer> list = new ArrayList<>();
         int size;
@@ -32,5 +63,5 @@ public class 크레인인형뽑기게임 {
         }
 
         return count;
-    }
+    }*/
 }
