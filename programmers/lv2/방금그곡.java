f97074462f01c0897f0f2cddf71bf562e9a23cf5
songyen <yeeun4150@gmail.com>
2021-04-14 22:48:37 +0900

    update Programmers [방금그곡]

--- programmers/lv2/방금그곡.java
+++ programmers/lv2/방금그곡.java
@@ -4,32 +4,33 @@ import java.util.*;
 
 public class 방금그곡 {
     public static void main(String[] args) {
-        System.out.println(solution("ABC", new String[]{"12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"}));
+        System.out.println(solution("CC#BCC#BCC#BCC#B", new String[]{"03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"}));
     }
     public static String solution(String m, String[] musicinfos) {
-        int mLength = m.length();
-        for(int i=0;i<m.length();i++){
-            if(m.charAt(i) == '#') mLength--;
-        }
-
-        int time, hour, min = 0;
+        m = sheetMusic(m);
+        int playingTime , hour, min = 0;
+        String sound = "";
+        List<Integer> select = new ArrayList<>();
         for(int i=0;i<musicinfos.length;i++){
+            sound = musicinfos[i].split(",")[3];
+            musicinfos[i] = musicinfos[i].replace(sound, sheetMusic(sound));
+            sound = musicinfos[i].split(",")[3];
+
             hour = Integer.parseInt(musicinfos[i].split(",")[1].split(":")[0]) - Integer.parseInt(musicinfos[i].split(",")[0].split(":")[0]);
             min = Integer.parseInt(musicinfos[i].split(",")[1].split(":")[1]) - Integer.parseInt(musicinfos[i].split(",")[0].split(":")[1]);
+            playingTime = hour*60 + min;
+            musicinfos[i] = musicinfos[i].replace(musicinfos[i].split(",")[0]+","+musicinfos[i].split(",")[1], Integer.toString(playingTime));
 
-            time = hour * 60 + min;
-            musicinfos[i] = musicinfos[i].replace(musicinfos[i].split(",")[0]+","+musicinfos[i].split(",")[1], Integer.toString(time));
-
-            if(mLength < time){
-                String sound = musicinfos[i].split(",")[2];
-                int sLength = sound.length();
-                for(int j=0;j<sound.length();j++) {
-                    if (sound.charAt(j) == '#') sLength--;
-                }
-
-                for(int k=0;k<time/sLength-1;k++) {
+            if(sound.length() < playingTime){
+                for(int j=0;j<playingTime/sound.length()-1;j++){
                     musicinfos[i] += sound;
                 }
+            }else{
+                musicinfos[i] = musicinfos[i].replace(sound, sound.substring(0,playingTime));
+            }
+
+            if(musicinfos[i].split(",")[2].contains(m)) {
+                select.add(i);
             }
         }
 
@@ -37,36 +38,32 @@ public class 방금그곡 {
             System.out.println(s);
         }
 
-        List<Integer> select = new ArrayList<>();
-        for(int i=0;i<musicinfos.length;i++){
-            if(musicinfos[i].split(",")[2].contains(m)){
-                System.out.println(i);
-                select.add(i);
-            }
-        }
-
+        int time, longPlayingTime = 0;
         String answer = "";
-        String playingTime = "";
         if(select.size() > 1){
-            playingTime = musicinfos[select.get(0)].split(",")[0];
-            for(int i=1;i<=select.size()-1;i++){
-                if(playingTime.equals(musicinfos[select.get(i)].split(",")[0])){
-                    answer = musicinfos[select.get(0)].split(",")[1];
+            longPlayingTime = Integer.parseInt(musicinfos[select.get(0)].split(",")[0]);
+            for(int i=1;i<select.size();i++){
+                time = Integer.parseInt(musicinfos[select.get(i)].split(",")[0]);
+                if(Integer.parseInt(musicinfos[select.get(i)].split(",")[0]) == Math.max(longPlayingTime,Integer.parseInt(musicinfos[select.get(i)].split(",")[0]))){
+                    longPlayingTime = Integer.parseInt(musicinfos[select.get(i)].split(",")[0]);
+                    answer = musicinfos[select.get(i)].split(",")[1];
                 }else{
-                    if(Integer.parseInt(musicinfos[select.get(i)].split(",")[0]) == Math.max(Integer.parseInt(playingTime),Integer.parseInt(musicinfos[select.get(i)].split(",")[0]))){
-                        answer = musicinfos[select.get(i)].split(",")[1];
-                        playingTime = musicinfos[select.get(i)].split(",")[1];
-                    }else{
-                        answer = musicinfos[select.get(0)].split(",")[1];
-                    }
+                    answer = musicinfos[select.get(0)].split(",")[1];
                 }
+
             }
-        }else if(select.size() == 0){
-            answer = "(None)";
-        }else{
+        }else if(select.size() == 1){
             answer = musicinfos[select.get(0)].split(",")[1];
+        }else{
+            answer = "(None)";
         }
 
         return answer;
+    }
+    public static String sheetMusic(String sound){
+        for(int i=0;i<sound.length();i++){
+            if(sound.charAt(i) == '#') sound = sound.replaceFirst("[A-G]#",sound.substring(i-1,i).toLowerCase());
+        }
+        return sound;
     }
 }
