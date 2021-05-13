9ac162c8637645d96cb22c03169a02b28ffd18fa
songyen <yeeun4150@gmail.com>
2021-05-13 22:30:56 +0900

    Programmers test4, test5

---
+++ programmers/lv2/Test4.java
@@ -0,0 +1,33 @@
+/*
+* Programmers 월간코드챌린지 시즌2 No.1 (100/100)
+ */
+package lv2;
+
+public class Test4 {
+    public static void main(String[] args) {
+        System.out.println(solution(13,17));
+    }
+    public static int solution(int left, int right) {
+        int sum = 0;
+        for(int i=left;i<=right;i++) {
+            if(even(i))sum+=i;
+            else sum -= i;
+        }
+        return sum;
+    }
+
+    public static boolean even(int num){
+        int count = 0;
+        for(int i=1;i*i<=num;i++){
+            if(num%i==0){
+                if(i*i==num) count++;
+                else {
+                    count++;
+                    count++;
+                }
+            }
+        }
+        if(count%2==0) return true;
+        else return false;
+    }
+}
---
+++ programmers/lv2/Test5.java
@@ -0,0 +1,34 @@
+/*
+ * Programmers 월간코드챌린지 시즌2 No.1 (54.6/100)
+ */
+package lv2;
+
+public class Test5 {
+    public static void main(String[] args) {
+        for(long l : solution(new long[]{2,7})){
+            System.out.println(l);
+        }
+    }
+    public static long[] solution(long[] numbers){
+        long[] answer = new long[numbers.length];
+        for(int i=0;i< numbers.length;i++){
+            answer[i] = function((int)numbers[i]);
+        }
+        return answer;
+    }
+    public static int function(int x){
+        for(int i=x+1; ;i++){
+            if(diff(Integer.toBinaryString(x^i))==3) continue;
+            else return i;
+        }
+    }
+
+    public static int diff(String xor){
+        int d = 0;
+        for(int i=0;i<xor.length();i++){
+            if(xor.charAt(i)=='1') d++;
+        }
+        if(d<=2) return 2;
+        else return 3;
+    }
+}
