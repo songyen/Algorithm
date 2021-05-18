d32ae070b39874250d25741bdf8367ddb7318ad1
songyen <yeeun4150@gmail.com>
2021-05-18 23:46:39 +0900

    update Programmers [JadenCase문자열만들기]

--- programmers/lv2/JadenCase문자열만들기.java
+++ programmers/lv2/JadenCase문자열만들기.java
@@ -1,24 +1,34 @@
-//package lv2;
-//
-//public class JadenCase문자열만들기 {
-//    public static void main(String[] args) {
-//        System.out.println(solution("1tart   you makyoue you    end"));
-//    }
-//    public static String solution(String s) {
-//        s = s.toLowerCase();
-//        String[] word = s.split("[\\s]+"); //공백문자가 1개 이상인 기준으로 split
-//        StringBuilder sb = new StringBuilder();
-//        String answer = "";
-//        String eachWord = "";
-//        for(int i=0;i<s.length();i++){
-//            if(s.charAt(i) == ' ')
-//            else{
-//                eachWord = word[i];
-//                eachWord = eachWord.replaceFirst(String.valueOf(word[i].charAt(0)), String.valueOf(word[i].charAt(0)).toUpperCase());
-//                answer += eachWord;
-//            }
-//        }
-//
-//        return answer;
-//    }
-//}
+/*
+* reference : https://velog.io/@ajufresh/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-JadenCase-%EB%AC%B8%EC%9E%90%EC%97%B4-%EB%A7%8C%EB%93%A4%EA%B8%B0-%EB%AC%B8%EC%A0%9C%ED%92%80%EC%9D%B4-Java
+ */
+package lv2;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class JadenCase문자열만들기 {
+    public static String solution(String s) {
+        s = s.toLowerCase();
+        StringBuilder answer = new StringBuilder();
+
+        String firstStr = s.charAt(0) + "";
+        answer.append(firstStr.toUpperCase()); // 첫 글자는 무조건 대문자
+
+        for (int i = 1; i < s.length(); i++) {
+            String now = s.charAt(i) + "";
+            if (now.equals(' ')) { // 공백이면 그대로 넘어가기
+                answer.append(" ");
+            }else if (s.charAt(i - 1) == ' ') { // 전 문자열이 공백이면
+                answer.append(now.toUpperCase()); // 대문자
+            }else{
+                answer.append(now);
+            }
+        }
+        return answer.toString();
+    }
+
+    @Test
+    public void 정답(){
+        Assert.assertEquals("3people Unfollowed Me", solution("3people unFollowed me"));
+    }
+}
