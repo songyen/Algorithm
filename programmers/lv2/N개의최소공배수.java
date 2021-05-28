cdbd4c3f59f66dd240d0d96225cfd253c0e6618b
songyen <yeeun4150@gmail.com>
2021-05-28 19:38:19 +0900

    update Programmers [N개의최소공배수]

--- programmers/lv2/N개의최소공배수.java
+++ programmers/lv2/N개의최소공배수.java
@@ -3,63 +3,38 @@ package lv2;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Arrays;
 
-public class N개의최소공배수 {
+public class N개의최소공배수{
+    public static void main(String[] args) {
+        N개의최소공배수 test = new N개의최소공배수();
+        System.out.println(test.solution(new int[]{2,6,8,14}));
+    }
     public int solution(int[] arr){
-        Arrays.sort(arr);
-
-        //나눠지는게 없을 때까지 구하는 거야
-        int divisor;
-        int divisorIndex;
-        int result = 1;
-        while(true){
-            divisorIndex = findDivisor(arr);
-            if(divisorIndex == -1) break;
-            else{
-                divisor = arr[divisorIndex];
-                result *= divisor;
-                for(int i=0;i<arr.length;i++){
-                    if(arr[i]%divisor != 0) arr[i] = arr[i];
-                    else arr[i] /= divisor;
-                }
-            }
+        int a = arr[0];
+        for(int i=1;i<arr.length;i++){
+            a = findLCM(a, arr[i]);
         }
-
-        //나눠지는 없으면 이제 곱하기
-        for(int i=0;i<arr.length;i++){
-            result *= arr[i];
-        }
-        return result;
+        return a;
     }
 
-    private int findDivisor(int[] arr) {
-        int divisor;
-        int divisorIndex = 0;
-        Loop1:
-        for(int i=0;i<arr.length;i++){
-            divisor = arr[i];
-            if(divisor == 1) continue;
-            if(i == arr.length-1) {
-                divisorIndex = -1;
-                break;
-            }
-
-            Loop2:
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[j]%divisor == 0){
-                    //나눠지는게 하나라도 있는지 확인 있으면 return divisor인덱스
-                    divisorIndex =  i;
-                    break Loop1;
-                }
-            }
-        }
-        return divisorIndex;
+    public int findGCD(int a, int b){
+        int r = a%b;
+        if(r==0) return b;
+        else return findGCD(b, r);
     }
 
-    @Test
-    public void test(){
-        Assert.assertEquals(168, solution(new int[]{2, 6, 8, 14}));
-        Assert.assertEquals(6, solution(new int[]{1,2,3}));
+    public int findLCM(int a, int b){
+
+        int gcd = findGCD(a, b);
+        return a*b/gcd;
     }
+
+
+
+
+//    @Test
+//    public void test(){
+//        Assert.assertEquals(168, solution(new int[]{2, 6, 8, 14}));
+//        Assert.assertEquals(6, solution(new int[]{1,2,3}));
+//    }
 }
