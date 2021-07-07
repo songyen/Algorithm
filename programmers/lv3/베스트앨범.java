e7e9df517f51bb5275d91795a8267b8258fcaf15
songyen <yeeun4150@gmail.com>
2021-07-07 17:53:26 +0900

    update Programmers [베스트앨범] redundant code 제거

--- programmers/lv3/베스트앨범.java
+++ programmers/lv3/베스트앨범.java
@@ -7,36 +7,39 @@ import java.util.*;
 
 public class 베스트앨범 {
     public int[] solution(String[] genres, int[] plays) {
+        //Step1
         HashMap<String, Integer> genreMap = new HashMap<>();
         for(int i=0;i<plays.length;i++){
-            genreMap.put(genres[i], genreMap.getOrDefault(genres[i],0)+plays[i]);//genreMap(장르, 재생수)
+            genreMap.put(genres[i], genreMap.getOrDefault(genres[i],0)+plays[i]);//genreMap(장르, 장르별 총 재생수)
         }
 
-        List<Map.Entry<String, Integer>> entries = new LinkedList<>(genreMap.entrySet());
-        Collections.sort(entries, (o1,o2)-> o2.getValue().compareTo(o1.getValue()));//장르 총 재생수 내림차순으로 정렬
-        LinkedHashMap<String, Integer> sortedMap = new LinkedHashMap<>();//map순서 보장
-        for(Map.Entry<String, Integer> entry : entries){
-            sortedMap.put(entry.getKey(), entry.getValue());
-        }
+        //Step2
+        List<Map.Entry<String, Integer>> genre_plays = new LinkedList<>(genreMap.entrySet());
+        Collections.sort(genre_plays, (o1, o2)-> o2.getValue().compareTo(o1.getValue()));//장르 총 재생수를 기준으로 내림차순 정렬
 
         HashMap<Integer, Integer> playsPerNum = new HashMap<>();
         List<Integer> answer = new ArrayList<>();
-        for(String genre : sortedMap.keySet()){
+        //Step3
+        for(Map.Entry<String, Integer> entry : genre_plays){
             for(int i=0;i<genres.length;i++){
-                if(genre.equals(genres[i])){
-                    playsPerNum.put(i, plays[i]);//장르별 (고유번호, 재생수) map 생성
+                if(entry.getKey().equals(genres[i])){
+                    playsPerNum.put(i, plays[i]);//장르별 (고유번호, 재생수) hashmap 생성
                 }
             }
 
-            List<Map.Entry<Integer, Integer>> sortedEntries = new LinkedList<>(playsPerNum.entrySet());
-            Collections.sort(sortedEntries, (o1,o2)-> o2.getValue().compareTo(o1.getValue()));//map(고유번호, 재생수)의 재생수 기준 내림차순으로 정렬(재생수 같다면, 고유번호 낮은 순으로 정렬)
+            //Step3-1
+            List<Map.Entry<Integer, Integer>> num_plays = new LinkedList<>(playsPerNum.entrySet());
+            Collections.sort(num_plays, (o1, o2)-> o2.getValue().compareTo(o1.getValue()));//playsPerNum(고유번호, 재생수)의 재생수 기준 내림차순으로 정렬(재생수 같다면, 고유번호 낮은 순으로 정렬)
 
-            answer.add(sortedEntries.get(0).getKey());
-            if(sortedEntries.size()!=1) answer.add(sortedEntries.get(1).getKey());
+            //Step3-2
+            answer.add(num_plays.get(0).getKey());
+            if(num_plays.size()!=1) answer.add(num_plays.get(1).getKey());
 
+            //Step3-3
             playsPerNum.clear();
         }
 
+        //Step4
         int[] result = new int[answer.size()];
         for(int i=0;i<answer.size();i++){
             result[i] = answer.get(i);
