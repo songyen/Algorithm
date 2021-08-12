adb945252878537d2f59873886b323888ad1fc00
songyen <yeeun4150@gmail.com>
2021-08-12 14:36:04 +0900

    Programmers [HIndex]

--- programmers/lv2/HIndex.java
+++ programmers/lv2/HIndex.java
@@ -1,23 +1,19 @@
 package lv2;
 
 import java.util.Arrays;
-import java.util.Collections;
 
 public class HIndex {
     public int solution(int[] citations) {
-        int answer = 0;
-        Integer[] arr = new Integer[citations.length];
-        for(int i=0;i<citations.length;i++){
-            arr[i] = citations[i];
-        }
-        Arrays.sort(arr, Collections.reverseOrder());
-
-        for(int i=0;i<citations.length;i++){
-            if(citations[i] <=i+1) {
-                answer = citations[i];
-                break;
+            int answer = 0;
+            Arrays.sort(citations);
+            for (int i = 0; i < citations.length; i++){
+                int h = citations.length - i;
+                if (citations[i] >= h) {
+                    answer = h;
+                    break;
+                }
             }
+            return answer;
         }
-        return answer;
     }
 }
