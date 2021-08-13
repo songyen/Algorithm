7f4aa3e8bdf05539cad715ec09906964c5ff5c96
songyen <yeeun4150@gmail.com>
2021-08-13 15:22:29 +0900

    rename BaekJoon [N15649 - BackTracking]

--- BaekJoon/Algorithm/BackTracking.java
+++ BaekJoon/Algorithm/BackTracking.java
@@ -1,7 +1,3 @@
-/*
- * reference : https://yongku.tistory.com/entry/%EB%B0%B1%EC%A4%80-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EB%B0%B1%EC%A4%80-15649%EB%B2%88-N%EA%B3%BC-M1-%EC%9E%90%EB%B0%94Java
- * source : BackJoon 15649 [N과 M(1)]
- */
 package Algorithm;
 
 import java.io.BufferedReader;
@@ -10,33 +6,34 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BackTracking {
-    private static int n,m;
-    private static int[] arr;
-    private static boolean[] visited;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        arr = new int[m];
-        visited = new boolean[n+1];
-        bt(0);
-    }
+        private static int n,m;
+        private static int[] arr;
+        private static boolean[] visited;
 
-    public static void bt(int cnt){
-        if(cnt == m){
-            for(int i : arr) System.out.print(i+" ");
-            System.out.println();
-            return;
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+            arr = new int[m];
+            visited = new boolean[n+1];
+            bt(0);
         }
 
-        for(int i=1;i<=n;i++){
-            if(!visited[i]){
-                visited[i] = true;
-                arr[cnt] = i;
-                bt(cnt+1);
-                visited[i] = false;
+        public static void bt(int cnt){
+            if(cnt == m){
+                for(int i : arr) System.out.print(i+" ");
+                System.out.println();
+                return;
+            }
+
+            for(int i=1;i<=n;i++){
+                if(!visited[i]){
+                    visited[i] = true;
+                    arr[cnt] = i;
+                    bt(cnt+1);
+                    visited[i] = false;
+                }
             }
         }
-    }
 }
