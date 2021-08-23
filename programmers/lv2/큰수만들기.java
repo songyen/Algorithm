223861f083831d3877ef53a60ed0d8484f4761f9
songyen <yeeun4150@gmail.com>
2021-08-23 16:46:39 +0900

    update Programmers [큰수만들기]

--- programmers/lv2/큰수만들기.java
+++ programmers/lv2/큰수만들기.java
@@ -1,45 +1,22 @@
+/*
+* reference : https://jhhj424.tistory.com/32
+ */
 package lv2;
 
-import java.util.Deque;
-import java.util.LinkedList;
-
 public class 큰수만들기 {
-    public static void main(String[] args) {
-        System.out.println(solution("1924",2));
-    }
-    private static int removeK;
-    public static String solution(String number, int k) {
-        removeK = k;
-        String answer = "";
-        int start = getStartIndex(number);
-        Deque<Integer> deque = new LinkedList<>();
-        deque.addLast(Integer.parseInt(number.charAt(start)+""));
-        for(int i=start+1;i<number.length();i++){
-            int num = Integer.parseInt(number.charAt(i)+"");
-            if(deque.peekLast() < num && removeK!=0) {
-                deque.pollLast();
-                removeK--;
+    public String solution(String number, int k){
+        StringBuilder sb = new StringBuilder();
+        int start = -1;
+        for(int i=0;i<number.length()-k;i++){
+            int max = 0;
+            for(int j=start+1;j<=k+i;j++){
+                if(max < number.charAt(j)-'0'){
+                    max = number.charAt(j)-'0';
+                    start = j;
+                }
             }
-            deque.addLast(num);
+            sb.append(max);
         }
-
-        for(int i : deque){
-            answer+= String.valueOf(i);
-        }
-        return answer;
-    }
-
-    public static int getStartIndex(String number){
-        int startIndex = 0;
-        int max = Integer.parseInt(number.charAt(0)+"");//1
-        for(int i=1;i<=removeK;i++){
-            int nextNum = Integer.parseInt(number.charAt(i)+"");
-            if(max < nextNum) {
-                max = nextNum;
-                startIndex = i;
-            }
-        }
-        removeK-=startIndex;
-        return startIndex;
+        return sb.toString();
     }
 }
