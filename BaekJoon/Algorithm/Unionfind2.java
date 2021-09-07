d9e03d86e16d9754bed7e418a1cd7bdf8558c04d
songyen <yeeun4150@gmail.com>
2021-09-07 23:22:33 +0900

    BaekJoon Unionfind [N4195 - 친구네트워크]

--- BaekJoon/Algorithm/Unionfind2.java
+++ BaekJoon/Algorithm/Unionfind2.java
@@ -1,30 +1,65 @@
+/*
+ * reference : https://youngest-programming.tistory.com/458
+ * source : BackJoon 4195번 [친구 네트워크]
+ */
 package Algorithm;
 
-import java.io.*;
+
 import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Unionfind2 {
-    static HashMap<String,String> friendNW;
-    static StringTokenizer st;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int t = Integer.parseInt(br.readLine());
-        friendNW = new HashMap<>();
-        for(int i=0;i<t;i++){
-            int F = Integer.parseInt(br.readLine());
-            st = new StringTokenizer(br.readLine());
-            for(int j=0;j<F;j++){
-                String a = st.nextToken().toUpperCase();
-                String b = st.nextToken().toUpperCase();
-//                if(friendNW.containsKey(a)||friendNW.containsKey(b)){
-//                    union(a,b);
-//                }
-                friendNW.put(a,a);
-                friendNW.put(b,a);
+    private static int T;
+    private static int F;
+    private static int[] parent;
+    private static int[] count;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        T = sc.nextInt();
+        for (int t = 0; t < T; t++) {
+            F = sc.nextInt();
+            parent = new int[F * 2];
+            count = new int[F * 2];
+            for (int i = 0; i < F * 2; i++) {
+                parent[i] = i;
+                count[i] = 1;
             }
-            friendNW.clear();
+            HashMap<String, Integer> map = new HashMap<>(); // 이름, 인덱스(노드번호)
+            int index = 0;
+            for (int f = 0; f < F; f++) {
+                String friend1 = sc.next();
+                String friend2 = sc.next();
+                if (!map.containsKey(friend1)) { //해당 친구이름이 아직 없는 경우 인덱스 부여
+                    map.put(friend1, index++);
+                }
+                if (!map.containsKey(friend2)) {
+                    map.put(friend2, index++);
+                }
+                System.out.println(union(map.get(friend1), map.get(friend2)));
+            }
         }
+    }
+
+    private static int find(int a) {
+        if (parent[a] == a) return a;
+        else return parent[a] = find(parent[a]);
+    }
+
+    private static int union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a != b) {
+            if (a < b) {
+                parent[b] = a; //집합 union
+                count[a] += count[b]; //친구수 union
+                return count[a];
+            } else {
+                parent[a] = b;
+                count[b] += count[a];
+                return count[b];
+            }
+        }
+        return count[a];
     }
 }
