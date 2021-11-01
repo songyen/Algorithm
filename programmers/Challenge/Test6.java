6b7cc631a2286592e36869bb684ba69a28dd61f9
songyen <yeeun4150@gmail.com>
2021-11-01 23:42:33 +0900

    BaekJoon 별찍기1_2438 [구현]

---
+++ programmers/Challenge/Test6.java
@@ -0,0 +1,14 @@
+package Challenge;
+
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Test6 {
+
+    public static void main(String[] args) {
+        String[] tmp = "10 3".split(" ");
+        System.out.println(Integer.parseInt(tmp[0]));
+    }
+
+}
