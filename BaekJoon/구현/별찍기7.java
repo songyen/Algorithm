52f9b4c8e4b12efd89706ff10045d7605e1113a0
songyen <yeeun4150@gmail.com>
2021-11-02 22:45:07 +0900

    Baekjoon 별찍기6_2444 [구현]

--- BaekJoon/구현/별찍기7.java
+++ BaekJoon/구현/별찍기7.java
@@ -4,16 +4,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class 별찍기6 {
+public class 별찍기7 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         br.close();
 
         StringBuilder sb = new StringBuilder();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++) sb.append(" ");
-            for(int j=1;j<=(n-i)*2-1;j++) sb.append("*");
+        for(int i=1;i<=n*2-1;i++){
+            if(i<=n){
+                for(int j=1;j<=n-i;j++) sb.append(" ");
+                for(int j=0;j<i*2-1;j++) sb.append("*");
+            }else{
+                for(int j=1;j<=i-n;j++) sb.append(" ");
+                for(int j=0;j<(n*2-i)*2-1;j++) sb.append("*");
+            }
             sb.append("\n");
         }
         System.out.print(sb);
