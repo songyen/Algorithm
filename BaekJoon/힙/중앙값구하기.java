64acb369443f5d0a22f45cb10444febc346da7a9
songyen <yeeun4150@gmail.com>
2021-11-05 22:42:53 +0900

    update Baekjoon 중앙값구하기_2696 통과

--- BaekJoon/힙/중앙값구하기.java
+++ BaekJoon/힙/중앙값구하기.java
@@ -12,25 +12,23 @@ public class 중앙값구하기 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int t = Integer.parseInt(br.readLine());
+        StringTokenizer st = null;
         for(int i=0;i<t;i++){
             StringBuilder sb = new StringBuilder();
             int n = Integer.parseInt(br.readLine());
             bw.write((n/2+1)+"\n");
-            StringTokenizer st;
             list = new ArrayList<>();
-            for(int j=0;j<=n/10;j++){
-                st = new StringTokenizer(br.readLine());
-                for(int k=1;k<=st.countTokens();k++){
-                    list.add(Integer.parseInt(st.nextToken()));
-                    if(k%2!=0) {
-                        sb.append(center()+" ");
-                        if((n/2+1)%10==0) {
-                            sb.append("\n");
-                        }
-                    }
+            int cnt = 0;
+            for(int j=0;j<n;j++){
+                if(j%10==0) st = new StringTokenizer(br.readLine());
+                list.add(Integer.parseInt(st.nextToken()));
+                if(j%2==0) {
+                    cnt++;
+                    if(cnt%10==0) sb.append(center()+"\n");
+                    else sb.append(center()+" ");
                 }
             }
-            bw.write(sb.toString().trim());
+            bw.write(sb.toString().trim()+"\n");
         }
         br.close();
         bw.flush();
