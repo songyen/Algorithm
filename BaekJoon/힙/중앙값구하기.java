aea0b9161c5993aa752c31fd85df0f46bee04201
songyen <yeeun4150@gmail.com>
2021-11-06 00:18:26 +0900

    update Baekjoon 중앙값구하기_2696 (최소힙, 스택 -> 최소힙, 최대힙) [힙]

--- BaekJoon/힙/중앙값구하기.java
+++ BaekJoon/힙/중앙값구하기.java
@@ -4,7 +4,8 @@ import java.io.*;
 import java.util.*;
 
 public class 중앙값구하기 {
-    private static PriorityQueue<Integer> pq;
+    private static PriorityQueue<Integer> maxHeap;
+    private static PriorityQueue<Integer> minHeap;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -15,14 +16,15 @@ public class 중앙값구하기 {
             int n = Integer.parseInt(br.readLine());
             bw.write((n/2+1)+"\n");
             int cnt = 0;
-            pq = new PriorityQueue<>();
+            maxHeap = new PriorityQueue<>((o1,o2)->o2-o1);
+            minHeap = new PriorityQueue<>();
             for(int j=0;j<n;j++){
                 if(j%10==0) st = new StringTokenizer(br.readLine());
-                pq.add(Integer.parseInt(st.nextToken()));
+                addHeap(Integer.parseInt(st.nextToken()));
                 if(j%2==0) {
                     cnt++;
-                    if(cnt%10==0) sb.append(center()+"\n");
-                    else sb.append(center()+" ");
+                    if(cnt%10==0) sb.append(maxHeap.peek()+"\n");
+                    else sb.append(maxHeap.peek()+" ");
                 }
             }
             bw.write(sb.toString().trim()+"\n");
@@ -32,16 +34,15 @@ public class 중앙값구하기 {
         bw.close();
     }
 
-    public static int center(){
-        Stack<Integer> stack = new Stack<>();
-        int size = pq.size();
-        for(int i=0;i<=size/2;i++){
-            stack.push(pq.poll());
+    private static void addHeap(int x) {
+        if(minHeap.size()== maxHeap.size()) maxHeap.add(x);
+        else minHeap.add(x);
+
+        if(!minHeap.isEmpty()) {
+            if (maxHeap.peek() > minHeap.peek()) {
+                minHeap.add(maxHeap.poll());
+                maxHeap.add(minHeap.poll());
+            }
         }
-        int center = stack.peek();
-        while(!stack.isEmpty()){
-            pq.add(stack.pop());
-        }
-        return center;
     }
 }
