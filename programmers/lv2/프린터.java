d780eea7b2c779ce0bb538115db5a626bbad673d
songyen <yeeun4150@gmail.com>
2021-11-16 21:49:20 +0900

    Programmers [프린터] review

--- programmers/lv2/프린터.java
+++ programmers/lv2/프린터.java
@@ -7,6 +7,26 @@ public class 프린터 {
         System.out.println(solution(new int[]{2,1,3,2},2));
     }
     public static int solution(int[] priorities, int location) {
+        /* 211116 review
+        Queue<Printer> queue = new LinkedList<>();
+        PriorityQueue<Integer> priority = new PriorityQueue<>((o1,o2)->o2-o1);//중요도 최대힙
+        for(int i=0;i<priorities.length;i++){
+            queue.add(new Printer(priorities[i], i));
+            priority.add(priorities[i]);
+        }
+        int answer = 0;
+        while(!queue.isEmpty()){
+            if(queue.peek().priority < priority.peek()){
+                queue.add(queue.poll());
+            }else{
+                if(queue.peek().index == location) break;
+                queue.poll();
+                priority.poll();
+                answer++;
+            }
+        }
+        return answer+1;
+         */
         Queue<Info> pool = new LinkedList<>();
         int index = 0;
         for(int i : priorities){
@@ -41,7 +61,17 @@ public class 프린터 {
 
         return answer;
     }
+    /* 211116 review
+    public static class Printer{
+        private int priority;
+        private int index;
 
+        public Printer(int priority, int index){
+            this.priority = priority;
+            this.index = index;
+        }
+    }
+    */
     static class Info{
         private int index;
         private int priority;
