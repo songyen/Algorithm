2e24da4f3ec4f82c5b90f54e3f7e201962e6cc06
songyen <yeeun4150@gmail.com>
2021-11-24 22:13:25 +0900

    Baekjoon N번째큰수_2693 [구현]

---
+++ BaekJoon/구현/N번째큰수.java
@@ -0,0 +1,28 @@
+package 구현;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class N번째큰수 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        while(T-->0){
+            int[] arr = new int[10];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int i=0;i<10;i++){
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(arr);
+            bw.write(arr[7]+"\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
