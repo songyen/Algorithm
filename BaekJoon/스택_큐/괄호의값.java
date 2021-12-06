57ebfb8a71331b7ca97e88c9437d2bfef383ce26
songyen <yeeun4150@gmail.com>
2021-12-06 17:39:17 +0900

    update Baekjoon 괄호의값_2504 [스택]

--- BaekJoon/스택_큐/괄호의값.java
+++ BaekJoon/스택_큐/괄호의값.java
@@ -1,62 +1,42 @@
 package 스택_큐;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Stack;
 
 public class 괄호의값 {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
+        int val = 1;
+        int result = 0;
         Stack<Character> stack = new Stack<>();
-        String result = "";
-        ArrayList<Integer> list = new ArrayList<>();
         for(int i=0;i<str.length();i++){
             char x = str.charAt(i);
-            char xNum;
-            if(stack.isEmpty()){
+
+            if(x=='(' || x=='['){
                 stack.push(x);
-                result+="0";
-                continue;
-            }
-            if (stack.peek() == '(' && x == ')') {
-                stack.pop();
-                if(stack.isEmpty()) {
-                    int sum =0;
-                    for (int j = 0; j < result.length(); j++) {
-                        if (result.charAt(j) != 0) sum += result.charAt(j) - '0';
-                    }
-                    result = "";
-                    list.add(sum *=2);
-                }else {
-                    xNum = result.charAt(result.length()-1);
-                    if (xNum != '0') result = result.substring(0,result.length()-1)+"0"+ (xNum - '0') * 2;
-                    else result += "2";
+                val*= (x=='(')?2:3;
+            }else if(x==')'){
+                if(stack.isEmpty() || stack.peek()!='(') {//올바르지 않은 괄호열
+                    result =0;
+                    break;
                 }
-            } else if (stack.peek() == '[' && x == ']') {
+                //올바른 괄호열
+                if(i-1>0 && str.charAt(i-1)=='(') result+=val;
                 stack.pop();
-                if(stack.isEmpty()) {
-                    int sum = 0;
-                    for (int j = 0; j < result.length(); j++) {
-                        if (result.charAt(j) != 0) sum += result.charAt(j) - '0';
-                    }
-                    result = "";
-                    list.add(sum *=3);
-                }else {
-                    xNum = result.charAt(result.length()-1);
-                    if (xNum != '0')  result = result.substring(0,result.length()-1)+"0"+ (xNum - '0') * 3;
-                    else result += "3";
+                val/=2;
+            }else{
+                if(stack.isEmpty() || stack.peek()!='[') {//올바르지 않은 괄호열
+                    result =0;
+                    break;
                 }
-            } else{
-                stack.push(x);
-                result += "0";
+                //올바른 괄호열
+                if(i-1>0 && str.charAt(i-1)=='[') result+=val;
+                stack.pop();
+                val/=3;
             }
         }
-        if(stack.isEmpty()) {
-            int answer = 0;
-            for(int i : list) answer+=i;
-            System.out.println(answer);
-        }
-        else System.out.println("0");
+        if(!stack.isEmpty()) result = 0;
+        System.out.println(result);
     }
 }
