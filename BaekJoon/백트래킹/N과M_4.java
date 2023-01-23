9148dd553c4a69b3d426db74ba40c97600bc04cc
songyen <yeeun4150@gmail.com>
2023-01-23 16:55:03 +0900

    Baekjoon 백트래킹_[N과M_4]

---
+++ BaekJoon/백트래킹/N과M_4.java
@@ -0,0 +1,28 @@
+package 백트래킹;
+
+import java.util.Scanner;
+
+public class N과M_4 {
+    private static int n,m;
+    private static int[] arr;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        arr = new int[m];
+        bfs(1,0);
+    }
+
+    private static void bfs(int start, int cnt) {
+        if(cnt==m){
+            for(int i : arr) System.out.print(i+" ");
+            System.out.println();
+            return;
+        }
+        for(int i=start;i<=n;i++){
+            arr[cnt] = i;
+            bfs(i,cnt+1);
+        }
+
+    }
+}
